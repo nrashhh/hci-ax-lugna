fdafa2eb3ef87a579562cb1777842e6014bf1a40
Naresh <geode858@gmail.com>
2018-11-25 21:03:57 +0800

    Made changes to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,8 @@ import { DataService, IDataItem } from "../core/data.service";
 @Component({
     selector: "Home",
     moduleId: module.id,
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
     constructor() {
