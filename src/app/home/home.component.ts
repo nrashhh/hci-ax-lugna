12b7c9d62d4464aa1484e29cb55d93ddd572ae96
Naresh <geode858@gmail.com>
2018-11-25 22:11:42 +0800

    Basic elements added to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,13 @@ import { DataService, IDataItem } from "../core/data.service";
     styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+    categoricalSource: { Country: string, Amount: number, SecondVal: number, ThirdVal: number }[] = [
+        { Country: "Jake", Amount: 15, SecondVal: 14, ThirdVal: 24 },
+        { Country: "Ahmad", Amount: 13, SecondVal: 23, ThirdVal: 25 },
+        { Country: "Ah Boon", Amount: 24, SecondVal: 17, ThirdVal: 23 },
+        { Country: "Preeti", Amount: 29, SecondVal: 112, ThirdVal: 29 }
+    ];
+
     constructor() {
         // Use the constructor to inject services.
     }
